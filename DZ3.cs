3b07d812522a2842abfca5abacc7a661efc4c802
Elijah <eligahbunn@gmail.com>
2022-08-23 22:41:53 +0300

    commit1

---
+++ DZ3.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Впишите номер четверти");
+int coord_plane = int.Parse(Console.ReadLine());
+
+if (coord_plane == 1)
+{
+    Console.WriteLine("Доступные координаты по оси Х: от 0 до ∞");
+    Console.WriteLine("Доступные координаты по оси У: от 0 до ∞");
+}
+else if(coord_plane == 2)
+{
+    Console.WriteLine("Доступные координаты по оси Х: от 0 до -∞");
+    Console.WriteLine("Доступные координаты по оси У: от 0 до ∞");
+}
+else if(coord_plane == 3)
+{
+    Console.WriteLine("Доступные координаты по оси Х: от 0 до -∞");
+    Console.WriteLine("Доступные координаты по оси У: от 0 до -∞");
+}
+else if(coord_plane == 4)
+{
+    Console.WriteLine("Доступные координаты по оси Х: от 0 до ∞");
+    Console.WriteLine("Доступные координаты по оси У: от 0 до -∞");
+}
+else
+{
+    Console.WriteLine("Введите адекватный номер коордитнатной пласкости");
+} 
