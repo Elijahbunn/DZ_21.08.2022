b31026328d149e6f521ee0cc21c4ce734cb96a76
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2022-08-23 22:45:12 +0300

    commit1

---
+++ DZ2.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int current1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int current2 = int.Parse(Console.ReadLine());
+
+if (current1 == current2*current2)
+{
+    Console.WriteLine("Первое число является квадрантом второго");
+}
+else if (current1 != current2*current2)
+{
+    Console.WriteLine("Первое число не является квадрантом второго");
+}
