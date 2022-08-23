1e389959c41d2e483ccd16f7105f126fd11ce4e3
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2022-08-23 22:44:56 +0300

    commit1

---
+++ DZ1.cs
@@ -0,0 +1,16 @@
+﻿int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+Console.WriteLine("Введите номер недели: ");
+int current = int.Parse(Console.ReadLine());
+
+if (current >= numbers[0] && current <= numbers[4] )
+{
+    Console.WriteLine("Введен номер буднего дня");
+}
+if (current >= numbers[5] && current <= numbers[6])
+{
+    Console.WriteLine("Введен номер выходного дня");
+}
+if (current > numbers[6])
+{
+    Console.WriteLine("Введен номер вне интервала");
+}
